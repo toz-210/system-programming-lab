71052814dbc022de12ff683f433c25ccd9830eea
ubuntu <ubuntu@computerlab.myguest.virtualbox.org>
2026-09-25 22:38:31 +0900

    I implement bp_list in break_cmd  and cont_cmd. In bp_list,I try to seperate the initalize and the other. In cont_cmd,I try to insert backup instruction through RIP register

--- debugger/debugger_x86.c
+++ debugger/debugger_x86.c
@@ -22,20 +22,32 @@ typedef struct list{
 	bool active;
 	struct list *next;
 	
-}bp_list;//about list
+}bp_list;//about list struct
 
-*bp_list create_list(*bp_list){
-	//if no list create list
-	//if already exist list create go to tail and create list  
+
+
+void run_cmd(char *address,pid_t pid,*bp_list head){//fork,traceme,exec,breakpoint active,execute until hit the breakpoint
+
 }
 
 
-*bp_list break_cmd(char *address,pid_t pid,*bp_list head){
-	
 
+
+*bp_list break_cmd(char *address,pid_t pid,*bp_list head){
 	unsigned long addr = strtout(address,NULL,0);//transfer string to addresss
 	bp_list *p=malloc(sizeof(bp_list));
-	p->address=addr
+	bp_list *current;//for saving current address
+	if(head==NULL){//if bp_list head doesn't exist
+		head=p;
+	}
+	else{//if bp_list head exist,go to end of list and chain the current list
+	current=head;
+	while(current->next!=NULL){
+		current=current->next;
+	}
+	current->next=p;//chain list to tail
+	}
+	p->address=addr;
 	p->bu_instr=ptrace(PTRACE_SEEKDATA,pid,addr,NULL);
 	p->active=1;
         p->next=NULL;
@@ -44,23 +56,21 @@ typedef struct list{
 	ptrace(PTRACE_GETREGS,pid,NULL,&regs);
 	regs.rip-=1;
 	ptrace(PTRACE_SETREGS,pid,NULL,&regs);//rip must be minus one  because int3 trigger RIP plus one for excuting next instruction so we need to decrease
-	return p;//return bp_list head 
-
-	
+	return head;//return bp_list head 	
 }
 
-void run_cmd(char *address,pid_t pid){
-	ptrace	
-}
-void cont_cmd(char *address,pid_t pid){
-	
-	ptrace(PTRACE_POKEDATA,pid,
-	ptrace(PTRACE_CONT,pid,NULL,NULL);
-	
+
+*bp_list cont_cmd(char *address,pid_t pid,*bp_list head){
+	struct user_regs_struct regs;
+	ptrace(PTRACE_GETREGS,pid,NULL,&regs);
+	unsigned long current=regs.rip;//current is register rip address
+	ptrace(PTRACE_POKEDATA,pid,current,head->bu_instr);//insert backup instruction into current address
+	ptrace(PTRACE_CONT,pid,NULL,NULL);//continue debugger
+	return head//return bp_list head
  
 }
 
-void info_cmd(char string[],pid_t pid){
+void info_cmd(char string[],pid_t pid,*bp_list head){
 
 	struct user_regs_struct regs;
 	if(strcmp("breakpoint",string)==0){
@@ -90,7 +100,7 @@ int main(int argc,char *argv[]){
 	
 	int status;//for process status
 	char buf[128];//it is buffer
-	
+	bp_list head=NULL;//for saving bp_lish head pointer and initalize head
 
 	while(1){
 	printf("mygdb)");
@@ -106,7 +116,6 @@ int main(int argc,char *argv[]){
 	if(pid==0){//child process 
 		ptrace(PTRACE_TRACEME,NULL,NULL);
 		execv(argv[1],&argv[1]);//runtarget	
-
 				}
 	else{//parent process(debugger)
 	     waitpid(pid,&status);
@@ -117,7 +126,7 @@ int main(int argc,char *argv[]){
 			     int n = sizeof(table)/sizeof(table[0]);
 			     for(int i=0;i<n; i++){
 				     if(strcmp(cmd,table[i].name)==0){
-					     table[i].handler(arg,pid,head);//parameter bp_list head
+					     head=table[i].handler(arg,pid,head);//parameter bp_list head
 					     break;
 				     }
 
@@ -126,13 +135,4 @@ int main(int argc,char *argv[]){
 			     }
 	     if(WIFEXITED(status)){//debugger finished
 		     printf("target exit\n");
-		     
 	     }
-
-
-			     
-	
-	
-	
-
-	
